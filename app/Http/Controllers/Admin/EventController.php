4960e9f97bd8eb6c0f9db1baa3859c79cddc668e
Kepin17 <servicenya4@gmail.com>
2026-07-25 17:18:22 +0700

    fix bug

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -12,7 +12,8 @@ class EventController extends Controller
     public function index()
     {
         $events = Event::latest()->paginate(10);
-        return view('admin.events.index', compact('events'));
+        $allEvents = Event::latest()->get();
+        return view('admin.events.index', compact('events', 'allEvents'));
     }
 
     public function create()
